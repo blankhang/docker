3de9e641f4058895f5aa8b55d9c5bbc0daac7cc2
blank <blankhang@gmail.com>
2021-07-21 16:01:58 +0800

    update jenkins

--- jenkins/docker-compose/init.sh
+++ jenkins/docker-compose/init.sh
@@ -4,13 +4,39 @@
 mkdir  -p /docker/jenkins/jenkins_home
 chmod 755 /docker/jenkins/jenkins_home
 
+# create jenkins's Dockerfile file
+cat << 'EOF' > /docker/jenkins/Dockerfile
+# https://github.com/jenkinsci/docker/blob/master/README.md
+FROM jenkins/jenkins:jdk11
+MAINTAINER blankhang@gmil.com
+
+user root
+
+# update system and install chinese language support
+RUN apt-get update && apt-get install -y maven nodejs \
+    && sed -i '/^#.* zh_CN.UTF-8 /s/^#//' /etc/locale.gen \
+    && locale-gen \
+    && rm -rf /var/lib/apt/lists/*
+
+# Setting Default Chinese Language
+ENV LANG: "zh_CN:UTF-8"
+ENV LANGUAGE: "zh_CN:zh"
+ENV LC_ALL: "zh_CN:UTF-8"
+
+user jenkins
+EOF
+
+
 # create jenkins's docker-compose file
 cat << 'EOF' > /docker/jenkins/docker-compose.yml
 version: '3.7'
 
 services:
   jenkins:
-    image: jenkinsci/blueocean
+    build:
+      dockerfile: Dockerfile
+      context: .
+    #image: jenkins/jenkins:jdk11
     container_name: jenkins
     restart: always
     privileged: true
@@ -20,15 +46,12 @@ services:
       interval: 1m
       timeout: 5s
       retries: 3
-      start_period: 3s
     environment:
       TZ : "Asia/Shanghai"
     ports:
       - '8080:8080'
     networks:
       - default
-#    extra_hosts:
-#      - "blankhang.t :177.177.177.177"
     volumes:
       - '/app:/app'
       - '/usr/share/fonts:/usr/share/fonts'
@@ -41,6 +64,9 @@ services:
 networks:
   default:
 EOF
+
+#build jenkins docker image
+docker-compose build
 
 echo -e "\033[36m init success \033[0m"
 echo "now you can run start.sh to start jenkins"
