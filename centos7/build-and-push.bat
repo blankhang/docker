a44e17561500502082a96606e96659cff51946f0
blankhang <blankhang@gmail.com>
2023-06-16 13:53:56 +0800

    update centos7 docker

--- centos7/build-and-push.bat
+++ centos7/build-and-push.bat
@@ -1,4 +1,4 @@
-SET IMAGE_NAME=blankhang/centos7
+SET IMAGE_NAME=blankhang/centos7-amd64
 
 docker build -t %IMAGE_NAME% .
 docker push %IMAGE_NAME%
--- centos7/build-and-push.sh
+++ centos7/build-and-push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_NAME=blankhang/centos7
+IMAGE_NAME=blankhang/centos7-arm64v8
 
 docker build -t ${IMAGE_NAME} .
 #docker push ${IMAGE_NAME}
