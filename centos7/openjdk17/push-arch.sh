4a6b57e2c62063be950706b07d81e189e9dcbdb8
blankhang <blankhang@gmail.com>
2023-06-16 14:58:52 +0800

    update centos7 docker Dockerfile

--- centos7/openjdk17/push-arch.sh
+++ centos7/openjdk17/push-arch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker manifest create blankhang/ubuntu2204:openjdk17 \
-blankhang/ubuntu2204:openjdk17-amd64 \
-blankhang/ubuntu2204:openjdk17-arm64v8
+docker manifest create blankhang/centos7:openjdk17 \
+blankhang/centos7:openjdk17-amd64 \
+blankhang/centos7:openjdk17-arm64v8
 
-docker manifest push blankhang/ubuntu2204:openjdk17
+docker manifest push blankhang/centos7:openjdk17
--- centos7/openjdk8/build-and-push.sh
+++ centos7/openjdk8/build-and-push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_NAME=blankhang/centos7:openjdk11-arm64v8
+IMAGE_NAME=blankhang/centos7:openjdk8-arm64v8
 
 docker build -t ${IMAGE_NAME} .
 docker push ${IMAGE_NAME}
