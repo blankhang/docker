c18a2fe0aa4be0c016a8ab5304c6ab4722926e08
blankhang <blankhang@gmail.com>
2023-06-16 13:31:55 +0800

    update ubuntu2204 openjdk11 arch script error

--- ubuntu2204/openjdk11/push-arch.sh
+++ ubuntu2204/openjdk11/push-arch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker manifest create blankhang/ubuntu2204:openjdk17 \
-blankhang/ubuntu2204:openjdk17-amd64 \
-blankhang/ubuntu2204:openjdk17-arm64v8
+docker manifest create blankhang/ubuntu2204:openjdk11 \
+blankhang/ubuntu2204:openjdk11-amd64 \
+blankhang/ubuntu2204:openjdk11-arm64v8
 
-docker manifest push blankhang/ubuntu2204:openjdk17
+docker manifest push blankhang/ubuntu2204:openjdk11
