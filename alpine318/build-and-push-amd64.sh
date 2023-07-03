fb8116e3e12c09a697d77988c06da6f481626ea7
blankhang <blankhang@gmail.com>
2023-07-04 01:36:02 +0800

    update docker alpine318 workflow

--- alpine318/build-and-push-amd64.sh
+++ alpine318/build-and-push-amd64.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_NAME=blankhang/alpine318:glibc-2.35-r1-amd64
+IMAGE_NAME=blankhang/alpine318-amd64
 
 docker build -t ${IMAGE_NAME} .
 docker push ${IMAGE_NAME}
--- alpine318/build-and-push.sh
+++ alpine318/build-and-push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_NAME=blankhang/alpine318:arm64v8
+IMAGE_NAME=blankhang/alpine318-arm64v8
 
 docker build -t ${IMAGE_NAME} .
 docker push ${IMAGE_NAME}
