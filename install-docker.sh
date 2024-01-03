2c5bec692d50cd4206edd8bd29036d7da0582494
blankhang <blankhang@gmail.com>
2024-01-03 10:24:19 +0800

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo '移除旧版 docker'
 sudo apt-get remove docker docker-engine docker-ce docker.io
 sudo apt-get update
@@ -11,7 +12,13 @@ curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key 
 sudo apt-get update
 
 echo '添加docker的远程库'
-add-apt-repository "deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+echo | sudo add-apt-repository "deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
 
 echo '安装 docker docker compose'
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo '添加 docker-compose 的 alias 指令'
+echo 'alias docker-compose="docker compose"' >> ~/.bashrc
+source ~/.bashrc
+
+echo '安装完成'
