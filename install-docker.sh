fdc01010a1b89cbb39180683b4bcefc8036431d6
blankhang <blankhang@gmail.com>
2025-06-11 17:09:43 +0800

    Update install-docker.sh
    change docker install mirrors to ustc

--- install-docker.sh
+++ install-docker.sh
@@ -7,10 +7,10 @@ sudo apt-get install -y apt-transport-https ca-certificates curl software-proper
 
 echo '创建 keyrings 目录并下载 Docker 的 GPG 公钥'
 sudo mkdir -p /etc/apt/keyrings
-sudo curl -fsSL https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 
 echo '添加 Docker 的远程库'
-echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 echo '安装 Docker 及相关插件'
 sudo apt-get update
