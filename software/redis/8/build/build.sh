4a88d7fa6cf6221463c7984d0bd079b5cdf07bb9
blank <blankhang@gmail.com>
2026-06-01 10:28:34 +0800

    redis 8.8.8 优化配置部署

--- software/redis/8/build/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-docker image build -t blankhang/redis:8 .
-
-docker push blankhang/redis:8
--- software/redis/8/docker-compose/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# start redis
-docker-compose -f /docker/redis/8/docker-compose.yml up -d
